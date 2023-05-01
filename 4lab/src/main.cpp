43ed03d7cf07855430f9ad9dbf5e592943e6efb5
Zuzanna <zuzaxmejer@gmail.com>
2023-05-01 20:36:10 +0200

    changed main

--- 4lab/src/main.cpp
+++ 4lab/src/main.cpp
@@ -3,6 +3,7 @@
 #include "parameters.hh"
 #include "RandomNumberGenerator.hh"
 #include "bruteforce.hh"
+#include "dynamic.hh"
 
 
 using namespace std::chrono;
@@ -17,13 +18,17 @@ int main()
     class_parameters.print(set);
     cout << class_parameters.evaluate(set) << endl << endl;
 
-    bruteforce class_brute;
-    cout << "Brutalna siła" << endl;
-    class_brute.init_brute(set);
-    class_parameters.print(class_brute.bestpi);
-    cout << "Delay: " << class_brute.ub << endl;
+    // bruteforce class_brute;
+    // cout << "Brutalna siła" << endl;
+    // class_brute.init_brute(set);
+    // class_parameters.print(class_brute.bestpi);
+    // cout << "Delay: " << class_brute.ub << endl;
 
-    cout << "Mateoda zachłanna" << endl;
-    class_parameters.print(class_parameters.sort(set));
-    cout << class_parameters.evaluate(class_parameters.sort(set)) << endl << endl;
+    // cout << "Mateoda zachłanna" << endl;
+    // class_parameters.print(class_parameters.sort(set));
+    // cout << class_parameters.evaluate(class_parameters.sort(set)) << endl << endl;
+
+    dynamic class_dynamic;
+    class_dynamic.dynamic_programming_init(set);
+    cout << class_dynamic.minimum << endl;
 }
